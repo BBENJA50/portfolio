ecbb253a3a39f272d942abe9eed430911bf1ee7c
Benjamin <migom@hotmail.be>
2023-11-09 16:17:27 +0100

    added 2 sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 // Import our custom CSS
 import '../scss/styles.scss'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+// ALLES HIER BOVEN INVOEGEN
 import '../scss/mycss.scss'
+
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
